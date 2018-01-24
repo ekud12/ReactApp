72f4ac1e1e37aedec1c03832944259907447cb4e
Macuser <macuser@admins-iMac.local>
2018-01-24 12:11:10 +0200

    spkit

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { AppRegistry, Text, StyleSheet, View ,Button, Alert,Vibration,TextInput} from 'react-native';
+import { TextInputWoW } from './TextInputWOW';
 
 export default class lotsOfTexts extends Component{
   render(){
@@ -31,28 +32,6 @@ export default class lotsOfTexts extends Component{
               title="Press Me"
             />
             </View>
-      </View>
-    );
-  }
-}
-
-class TextInputWoW extends Component {
-  constructor(props){
-    super(props);
-    this.state = { text: ''};
-  }
-
-  render() {
-    return (
-      <View style={{padding: 10}}>
-        <TextInput
-          style={{height: 40,width:130, borderColor:'white', borderWidth:1}}
-          placeholder="Type here to translate!"
-          onChangeText={(text) => this.setState({text})}
-        />
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-        </Text>
       </View>
     );
   }
